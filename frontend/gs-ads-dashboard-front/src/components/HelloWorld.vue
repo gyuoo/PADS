cfb9fac4d33c80fb96aade25746edf24361cc654
전상현 <breakking@naver.com>
2024-10-24 12:42:25 +0900

    feat: vue setting

---
+++ frontend/gs-ads-dashboard-front/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Button from './ui/button/Button.vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <Button class="ms-3" type="button" @click="count++"
+      >count is {{ count }}</Button
+    >
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
