11cf23159e9b9dadc7e64b5bbbcca6c901a609c2
전상현 <breakking@naver.com>
2024-10-28 15:49:44 +0900

    Remove : 기존 react 프로젝트 삭제

--- frontend/gs-ads-dashboard/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './styles/global.css'
-createRoot(document.getElementById('root')!).render(
-  <>
-    <App />
-  </>,
-)
