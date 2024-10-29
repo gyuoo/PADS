c934ea417b85c231ccf4089bdb1467ec169d5edb
염규영 <yeomky98@naver.com>
2024-10-29 18:05:04 +0900

    Feat: Add method to find User by refresh token

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/repository/UserRepository.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/repository/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByRefreshToken(String refreshToken);
+
 }
