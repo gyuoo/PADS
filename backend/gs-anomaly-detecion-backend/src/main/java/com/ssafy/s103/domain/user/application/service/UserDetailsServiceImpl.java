c40b4509fe9eb71b55b5c0e55c4d4547288e5270
염규영 <yeomky98@naver.com>
2024-10-29 16:06:47 +0900

    fix: correct implementation issues

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserDetailsServiceImpl.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserDetailsServiceImpl.java
@@ -5,15 +5,17 @@ import com.ssafy.s103.domain.user.entity.User;
 import com.ssafy.s103.domain.user.entity.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
             .orElseThrow(() -> new IllegalArgumentException(email));
