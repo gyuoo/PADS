189446b25c4f437e15c5d70545b4448ea2e3f671
염규영 <yeomky98@naver.com>
2024-10-29 15:55:27 +0900

    feat: add UserDetailsService

---
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserDetailsService.java
@@ -0,0 +1,22 @@
+package com.ssafy.s103.domain.user.application.service;
+
+import com.ssafy.s103.domain.user.application.repository.UserRepository;
+import com.ssafy.s103.domain.user.entity.User;
+import com.ssafy.s103.domain.user.entity.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new IllegalArgumentException(email));
+        return new UserDetailsImpl(user);
+    }
+}
