fc562df94889ac0b6511e55363b3556c45a04ff8
염규영 <yeomky98@naver.com>
2024-10-29 15:55:59 +0900

    feat: add UserRepository

---
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.s103.domain.user.application.repository;
+
+import com.ssafy.s103.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
