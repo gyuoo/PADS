7d190a88d6452bb6107d92cc5705b079affa4534
gyeong_min <nkh9587@naver.com>
2024-10-30 13:37:57 +0900

    Feat: DashboardMain 컴포넌트 추가

--- frontend/gs-ads-dashboard-front/src/components/dashboard/DashboardMain.vue
+++ frontend/gs-ads-dashboard-front/src/components/dashboard/DashboardMain.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>대시보드 메인 컴포넌트</div>
+  <div class="flex h-[565px]">
+    <div class="flex flex-col gap-2 w-1/5 h-full p-2">
+      <OverviewCount label="전체 상품 수" :count="16534672" />
+      <OverviewCount label="대기 중인 상품 수" :count="1324" />
+      <DoughnutChart />
+    </div>
+    <div class="w-4/5 h-full overflow-y-auto p-2">
+      <AnomalyProductList />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AnomalyProductList from './AnomalyProductList.vue';
+import OverviewCount from './OverviewCard.vue';
+import DoughnutChart from './DoughnutChart.vue';
+</script>
 
 <style scoped></style>
