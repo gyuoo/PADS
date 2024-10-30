9ea4a5ff5841c1656bfc7f8d44bf1f187cff9b5b
gyeong_min <nkh9587@naver.com>
2024-10-30 13:36:21 +0900

    Feat: OverviewCard 컴포넌트 추가

---
+++ frontend/gs-ads-dashboard-front/src/components/dashboard/OverviewCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="count-card bg-white rounded-lg shadow-lg p-4 w-full my-2">
+    <div class="text-sm text-gray-500 mb-2">{{ label }}</div>
+    <div class="text-3xl font-bold text-right break-words">{{ count.toLocaleString() }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  label: string;
+  count: number;
+}>();
+</script>
+
+<style scoped></style>
