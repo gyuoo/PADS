d3d2be303aac048b064f70db01149bbe4c0095af
염규영 <yeomky98@naver.com>
2024-10-31 17:10:26 +0900

    Refactor: Add redirect url field

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/response/ApiResponse.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/response/ApiResponse.java
@@ -5,21 +5,23 @@ import org.springframework.http.HttpStatus;
 public record ApiResponse<T>(
     String message,
     HttpStatus status,
-    T data) {
+    T data,
+    String redirectUrl) {
 
-    public static <T> ApiResponse<T> of(String message, HttpStatus httpStatus, T data) {
-        return new ApiResponse<>(message, httpStatus, data);
+    public static <T> ApiResponse<T> of(String message, HttpStatus httpStatus, T data,
+        String redirectUrl) {
+        return new ApiResponse<>(message, httpStatus, data, redirectUrl);
     }
 
     public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
-        return new ApiResponse<>(null, httpStatus, data);
+        return new ApiResponse<>(null, httpStatus, data, null);
     }
 
     public static <T> ApiResponse<T> of(String message, HttpStatus httpStatus) {
-        return new ApiResponse<>(message, httpStatus, null);
+        return new ApiResponse<>(message, httpStatus, null, null);
     }
 
     public static <T> ApiResponse<T> of(HttpStatus httpStatus) {
-        return new ApiResponse<>(null, httpStatus, null);
+        return new ApiResponse<>(null, httpStatus, null, null);
     }
 }
