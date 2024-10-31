493b3638c56225d06de9e2eaab62c29cada0d5d2
염규영 <yeomky98@naver.com>
2024-10-31 16:53:16 +0900

    Refactor: Update security configuration to use Redis and custom handlers

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/config/SecurityConfig.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/config/SecurityConfig.java
@@ -1,120 +1,72 @@
 package com.ssafy.s103.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.s103.domain.user.application.repository.UserRepository;
-import com.ssafy.s103.global.security.filter.JsonUsernamePasswordAuthenticationFilter;
-import com.ssafy.s103.global.security.filter.JwtAuthenticationProcessingFilter;
-import com.ssafy.s103.global.security.handler.LoginFailureHandler;
-import com.ssafy.s103.global.security.handler.LoginSuccessJwtProvideHandler;
-import com.ssafy.s103.global.security.service.JwtService;
+import com.ssafy.s103.domain.user.application.service.UserDetailsServiceImpl;
+import com.ssafy.s103.global.security.handler.CustomLoginFailureHandler;
+import com.ssafy.s103.global.security.handler.CustomLoginSuccessHandler;
+import com.ssafy.s103.global.security.service.RedisTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
-    private final ObjectMapper objectMapper;
-    private final UserRepository userRepository;
-    private final JwtService jwtService;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final CustomLoginSuccessHandler customLoginSuccessHandler;
+    private final CustomLoginFailureHandler customLoginFailureHandler;
 
     @Bean
-    public PasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityfilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .authorizeHttpRequests(auth -> auth //인증, 인가 설정
+                .requestMatchers(
+                    new AntPathRequestMatcher("/login"),
+                    new AntPathRequestMatcher("/register"),
+                    new AntPathRequestMatcher("/")
+                ).permitAll()
+                .anyRequest().authenticated())
+            .formLogin(formLogin -> formLogin
+                .loginPage("/login")
+                .successHandler(customLoginSuccessHandler)
+                .failureHandler(customLoginFailureHandler)
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+            )
+            .csrf(AbstractHttpConfigurer::disable)
+            .build();
     }
 
     @Bean
-    public SecurityFilterChain securityfilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
-            .httpBasic(AbstractHttpConfigurer::disable) // HTTP basic 비활성화
-            .formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
-            .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/register", "/", "/login").permitAll() // 해당 경로에 대한 요청은 permit
-                .anyRequest().authenticated()) // 그 외의 모든 요청은 인증된 사용자만 접근 가능
-            .logout((logout) -> logout
-                .logoutSuccessUrl("/login") // 로그아웃 시 리다이렉트
-                .invalidateHttpSession(true))
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 서버에서 세션을 관리하지 않음
-            );
-        http
-            .addFilterAfter(jsonUsernamePasswordLoginFilter(),
-                LogoutFilter.class) // 커스터마이징 된 필터를 SpringSecurityFilterChain에 등록
-            .addFilterBefore(jwtAuthenticationProcessingFilter(),
-                JsonUsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
-
-    /*
-     현재 사용되는 알고리즘에서 다른 인코딩 알고리즘으로 변경하고자 할 때 이용
-     여러 인코딩 알고리즘을 사용할 수 있게 해줌
-     */
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
+    public DaoAuthenticationProvider authProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsServiceImpl);
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
 
         return daoAuthenticationProvider;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() { // AuthenticationManager 등록
-        DaoAuthenticationProvider provider = daoAuthenticationProvider(); // DaoAuthenticationProvider 사용
-        provider.setPasswordEncoder(
-            passwordEncoder()); // PasswordEncoder로는 PasswordEncoderFactories.createDelegatingPasswordEncoder() 사용
-        return new ProviderManager(provider);
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    // Redis
     @Bean
-    public LoginSuccessJwtProvideHandler loginSuccessJwtProvideHandler() {
-        return new LoginSuccessJwtProvideHandler(jwtService, userRepository);
-    }
-
-    @Bean
-    public LoginFailureHandler loginFailureHandler() {
-        return new LoginFailureHandler();
-    }
-
-    @Bean
-    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter() {
-        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter = new JsonUsernamePasswordAuthenticationFilter(
-            objectMapper);
-        jsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
-        jsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(
-            loginSuccessJwtProvideHandler());
-        jsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
-        return jsonUsernamePasswordLoginFilter;
-    }
-
-    @Bean
-    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
-        JwtAuthenticationProcessingFilter jsonUsernamePasswordLoginFilter = new JwtAuthenticationProcessingFilter(
-            jwtService, userRepository);
-
-        return jsonUsernamePasswordLoginFilter;
+    public PersistentTokenRepository persistentTokenRepository() {
+        return new RedisTokenService(redisTemplate);
     }
 }
-
