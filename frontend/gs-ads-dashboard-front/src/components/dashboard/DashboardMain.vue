aa9d16196c48c479d305f739df14782dff1c2416
gyeong_min <nkh9587@naver.com>
2024-10-31 11:28:17 +0900

    Style: 크기 조정

--- frontend/gs-ads-dashboard-front/src/components/dashboard/DashboardMain.vue
+++ frontend/gs-ads-dashboard-front/src/components/dashboard/DashboardMain.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex h-[560px]">
-    <div class="flex flex-col gap-3 w-1/5 h-full p-2">
+  <div class="flex flex-col md:flex-row h-full">
+    <div class="flex flex-col gap-3 w-full md:w-1/5 h-full p-2">
       <OverviewCount label="전체 상품 수" :count="16534672" />
       <OverviewCount label="대기 중인 상품 수" :count="1324" />
       <DoughnutChart />
     </div>
-    <div class="w-4/5 h-full overflow-y-auto p-2">
-      <AnomalyProductList />
+    <div class="w-full md:w-4/5 h-full p-2">
+      <AnomalyProductList class="h-full" />
     </div>
   </div>
 </template>
@@ -16,5 +16,3 @@ import AnomalyProductList from './AnomalyProductList.vue';
 import OverviewCount from './OverviewCard.vue';
 import DoughnutChart from './DoughnutChart.vue';
 </script>
-
-<style scoped></style>
