6215b576c0902c16c328c18df8c2f28f7ae2c7e8
gyeong_min <nkh9587@naver.com>
2024-11-01 09:04:52 +0900

    Feat: 상품 상세 부모 컴포넌트

---
+++ frontend/gs-ads-dashboard-front/src/components/product/ProductDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col md:flex-row">
+        <div class="flex flex-col gap-3 w-full md:w-1/2 h-full p-2">
+            <ProductInfo />
+        </div>
+        <div class="flex flex-col gap-3 w-full md:w-1/2 h-full p-2">
+            <ProductAnomalyReport />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ProductAnomalyReport from './ProductAnomalyReport .vue';
+import ProductInfo from './ProductInfo.vue';
+
+
+</script>
+
+<style scoped></style>
