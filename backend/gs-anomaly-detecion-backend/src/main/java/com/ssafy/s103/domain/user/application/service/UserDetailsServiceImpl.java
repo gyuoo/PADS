f40fdc65e06cc8cca446d2453889e45a452baf36
염규영 <yeomky98@naver.com>
2024-11-01 16:44:09 +0900

    Refactor: Change UserDetailsServiceImpl to return CustomUser instance

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserDetailsServiceImpl.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserDetailsServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ssafy.s103.domain.user.application.service;
 
 import com.ssafy.s103.domain.user.application.repository.UserRepository;
+import com.ssafy.s103.domain.user.entity.CustomUser;
 import com.ssafy.s103.domain.user.entity.User;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public User loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-            .orElseThrow(() -> new IllegalArgumentException(email));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        return user.map(CustomUser::new).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
