829129136c0980654c0aa10e05987d3cf30db1ec
염규영 <yeomky98@naver.com>
2024-11-01 16:55:59 +0900

    Refactor: Simplify UserService

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserService.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/user/application/service/UserService.java
@@ -4,7 +4,6 @@ import com.ssafy.s103.domain.user.application.repository.UserRepository;
 import com.ssafy.s103.domain.user.dto.request.UserRegisterRequestDto;
 import com.ssafy.s103.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,25 +15,25 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserDetails save(UserRegisterRequestDto userRegisterRequestDto) {
-        return userRepository.save(User.builder()
-            .email(userRegisterRequestDto.email())
-            .password(bCryptPasswordEncoder.encode(userRegisterRequestDto.password()))
-            .build());
-    }
-
-    public boolean login(String rawPassword, UserRegisterRequestDto userRegisterRequestDto) {
-        User user = userRepository.findByEmail(userRegisterRequestDto.email())
-            .orElseThrow(() -> new UsernameNotFoundException(userRegisterRequestDto.email()));
-        if (user != null) {
-            return passwordChecker(rawPassword, user.getPassword());
-        } else {
-            throw new UsernameNotFoundException(userRegisterRequestDto.email());
+    public void save(UserRegisterRequestDto userRegisterRequestDto) {
+        try {
+            userRepository.save(User.builder()
+                .email(userRegisterRequestDto.email())
+                .password(bCryptPasswordEncoder.encode(userRegisterRequestDto.password()))
+                .build());
+        } catch (Exception e) {
+            throw new UsernameNotFoundException(e.getMessage());
         }
     }
 
-    public boolean passwordChecker(String rawPassword, String hashedPassword) {
-        return bCryptPasswordEncoder.matches(rawPassword, hashedPassword);
-    }
+//    public boolean login(String rawPassword, UserLoginRequestDto userLoginRequestDto) {
+//        User user = userRepository.findByEmail(userLoginRequestDto.email())
+//            .orElseThrow(() -> new UsernameNotFoundException(userLoginRequestDto.email()));
+//        return passwordChecker(rawPassword, user.getPassword());
+//    }
+//
+//    public boolean passwordChecker(String rawPassword, String hashedPassword) {
+//        return bCryptPasswordEncoder.matches(rawPassword, hashedPassword);
+//    }
 
 }
