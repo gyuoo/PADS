10b70bf62d7600dba7162a89bc94b059d635a52f
gyeong_min <nkh9587@naver.com>
2024-11-01 09:03:01 +0900

    Feat: 상품 상세 리포트 라우터 설정

--- frontend/gs-ads-dashboard-front/src/router/index.ts
+++ frontend/gs-ads-dashboard-front/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Main from '@/views/dashboard/Main.vue'
 import ProductMain from '@/views/product/ProductMain.vue'
+import ProductDetailMain from '@/views/product/ProductDetailMain.vue'
 // 라우터 설정
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'product',
     component: ProductMain,
   },
+  {
+    path: '/product/:id',
+    name: 'ProductDetail',
+    component: ProductDetailMain,
+  }
 ]
 
 // 라우터 생성
