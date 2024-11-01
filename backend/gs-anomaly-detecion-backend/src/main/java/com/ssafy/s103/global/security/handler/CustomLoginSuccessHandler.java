7c7cc9fdf274aa58693449f987cc3d45a36c7c94
염규영 <yeomky98@naver.com>
2024-11-01 16:44:59 +0900

    Chore: Add logging for login success

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/handler/CustomLoginSuccessHandler.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/handler/CustomLoginSuccessHandler.java
@@ -5,11 +5,13 @@ import com.ssafy.s103.global.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
@@ -17,6 +19,8 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException {
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        log.info(SuccessCode.LOGIN_SUCCESS.getMessage());
 
         ApiResponse<Void> apiResponse = ApiResponse.of(
             SuccessCode.LOGIN_SUCCESS.getMessage(),
