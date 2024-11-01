21f1da0a53ae9c43a201cd85c0f93c86db4a0135
염규영 <yeomky98@naver.com>
2024-11-01 16:45:15 +0900

    Chore: Change logging for login failure

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/handler/CustomLoginFailureHandler.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/handler/CustomLoginFailureHandler.java
@@ -1,11 +1,11 @@
 package com.ssafy.s103.global.security.handler;
 
+import com.ssafy.s103.global.exception.ErrorCode;
 import com.ssafy.s103.global.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -18,14 +18,14 @@ public class CustomLoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException exception) throws IOException {
 
-        log.error("Login failed: {}", exception.getMessage());
+        log.error(ErrorCode.LOGIN_FAILED.getMessage());
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
 
         ApiResponse<Void> apiResponse = ApiResponse.of(
-            exception.getMessage(),
-            HttpStatus.UNAUTHORIZED,
+            ErrorCode.LOGIN_FAILED.getMessage(),
+            ErrorCode.LOGIN_FAILED.getCode(),
             null,
             null
         );
