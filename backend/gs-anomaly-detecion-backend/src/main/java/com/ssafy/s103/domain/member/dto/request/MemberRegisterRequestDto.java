4cd74ae70b7d6f425f22cbcc6f4c4aebf5ebdb0e
염규영 <yeomky98@naver.com>
2024-11-04 17:00:04 +0900

    Refactor: Add toEntity method to MemberRegisterRequestDto

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/dto/request/MemberRegisterRequestDto.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/dto/request/MemberRegisterRequestDto.java
@@ -1,10 +1,18 @@
 package com.ssafy.s103.domain.member.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record MemberRegisterRequestDto(
     @NotNull String email,
     @NotNull String password
 ) {
 
+    public MemberRegisterRequestDto toEntity(String email, String password) {
+        return MemberRegisterRequestDto.builder()
+            .email(email)
+            .password(password)
+            .build();
+    }
 }
