1a0e6489e6fde20201748df1b9660f66b29f2cff
염규영 <yeomky98@naver.com>
2024-11-04 14:42:56 +0900

    Feat: Add success codes

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/exception/SuccessCode.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/exception/SuccessCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
     LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공하였습니다."),
     REGISTER_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다."),
-    LOGOUT_SUCCESS(HttpStatus.OK, "성공적으로 로그아웃 되었습니다.");
+    LOGOUT_SUCCESS(HttpStatus.OK, "성공적으로 로그아웃 되었습니다."),
+    SEND_MAIL_SUCCESS(HttpStatus.OK, "성공적으로 메일을 전송하였습니다."),
+    VERIFICATION_SUCCESS(HttpStatus.OK, "이메일 인증에 성공하였습니다.");
 
     private final HttpStatus code;
     private final String message;
