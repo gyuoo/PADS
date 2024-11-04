66fceb1821b3367548fa1ffcc12660e86bd5e4f5
gyeong_min <nkh9587@naver.com>
2024-11-04 17:39:50 +0900

    Feat: AnomalyLog entity 추가

---
+++ backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/entity/AnomalyLog.java
@@ -0,0 +1,44 @@
+package com.ssafy.s103.domain.anomaly.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnomalyLog {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Long productId;
+
+	@Column(nullable = false)
+	private String anomalyCode;
+
+	@Column(nullable = false)
+	private Integer anomalyScore;
+
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@Builder
+	public AnomalyLog(Long productId, String anomalyCode, Integer anomalyScore) {
+		this.anomalyCode = anomalyCode;
+		this.productId = productId;
+		this.anomalyScore = anomalyScore;
+	}
+}
