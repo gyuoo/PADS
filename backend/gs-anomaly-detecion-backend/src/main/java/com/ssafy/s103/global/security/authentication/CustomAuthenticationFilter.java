f0bdf4b9d81f784d69d8d0d242dbee206741cc19
염규영 <yeomky98@naver.com>
2024-11-05 14:22:18 +0900

    Feat: Add custom authentication filter for login endpoint

---
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/authentication/CustomAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.ssafy.s103.global.security.authentication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.s103.domain.member.dto.request.MemberLoginRequestDto;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+public class CustomAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public CustomAuthenticationFilter() {
+        // url과 일치할 경우 해당 필터가 동작합니다.
+        super(new AntPathRequestMatcher("/api/v1/members/login"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+        HttpServletResponse response)
+        throws AuthenticationException, IOException, ServletException {
+
+        // 해당 요청이 POST 인지 확인
+        if (!isPost(request)) {
+            throw new IllegalStateException("Authentication is not supported");
+        }
+
+        // POST 이면 body 를 AccountDto( 로그인 정보 DTO ) 에 매핑
+        MemberLoginRequestDto memberLoginRequestDto = objectMapper.readValue(request.getReader(),
+            MemberLoginRequestDto.class);
+
+        // ID, PASSWORD 가 있는지 확인
+        if (!StringUtils.hasLength(memberLoginRequestDto.email())
+            || !StringUtils.hasLength(memberLoginRequestDto.password())) {
+            throw new IllegalArgumentException("username or password is empty");
+        }
+
+        // 처음에는 인증 되지 않은 토큰 생성
+        CustomAuthenticationToken token = new CustomAuthenticationToken(
+            memberLoginRequestDto.email(),
+            memberLoginRequestDto.password()
+        );
+
+        // Manager 에게 인증 처리
+        Authentication authenticate = getAuthenticationManager().authenticate(token);
+
+        return authenticate;
+    }
+
+    private boolean isPost(HttpServletRequest request) {
+        return "POST".equals(request.getMethod());
+    }
+}
