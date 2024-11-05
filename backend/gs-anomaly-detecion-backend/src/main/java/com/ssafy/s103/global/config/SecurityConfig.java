e807f1d05ec7c351026102d489dd6e90f22ae535
염규영 <yeomky98@naver.com>
2024-11-05 14:26:32 +0900

    Refactor: Update SecurityConfig

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/config/SecurityConfig.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/config/SecurityConfig.java
@@ -1,62 +1,68 @@
 package com.ssafy.s103.global.config;
 
-import com.ssafy.s103.domain.member.application.service.MemberDetailsService;
+import com.ssafy.s103.global.security.authentication.CustomAuthenticationFilter;
+import com.ssafy.s103.global.security.handler.CustomAccessDeniedHandler;
+import com.ssafy.s103.global.security.handler.CustomLoginAuthenticationEntryPoint;
 import com.ssafy.s103.global.security.handler.CustomLoginFailureHandler;
 import com.ssafy.s103.global.security.handler.CustomLoginSuccessHandler;
-import com.ssafy.s103.global.security.service.RedisTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final MemberDetailsService memberDetailsService;
     private final CustomLoginSuccessHandler customLoginSuccessHandler;
     private final CustomLoginFailureHandler customLoginFailureHandler;
+    private final CustomLoginAuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
-    public SecurityFilterChain securityfilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-            .authorizeHttpRequests(auth -> auth //인증, 인가 설정
-                .requestMatchers(
-                    new AntPathRequestMatcher("/members/login"),
-                    new AntPathRequestMatcher("/members/register"),
-                    new AntPathRequestMatcher("/")
-                ).permitAll()
-                .anyRequest().authenticated())
-            .formLogin(formLogin -> formLogin
-                .loginPage("/members/login")
-                .successHandler(customLoginSuccessHandler)
-                .failureHandler(customLoginFailureHandler)
-            )
-            .logout(logout -> logout
-                .logoutSuccessUrl("/members/login")
-                .invalidateHttpSession(true)
-            )
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
             .csrf(AbstractHttpConfigurer::disable)
-            .build();
+            .cors(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> request
+                .requestMatchers(
+                    "/api/v1/members/**",
+                    "/"
+                ).permitAll())
+//                .anyRequest().authenticated())
+            .addFilterBefore(ajaxAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(config -> config
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler));
+        return httpSecurity.build();
     }
 
     @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(memberDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+    public CustomAuthenticationFilter ajaxAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter();
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        customAuthenticationFilter.setAuthenticationSuccessHandler(customLoginSuccessHandler);
+        customAuthenticationFilter.setAuthenticationFailureHandler(customLoginFailureHandler);
 
-        return daoAuthenticationProvider;
+        customAuthenticationFilter.setSecurityContextRepository(
+            new DelegatingSecurityContextRepository(
+                new RequestAttributeSecurityContextRepository(),
+                new HttpSessionSecurityContextRepository()
+            ));
+
+        return customAuthenticationFilter;
     }
 
     @Bean
@@ -64,9 +70,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // Redis
     @Bean
-    public PersistentTokenRepository persistentTokenRepository() {
-        return new RedisTokenService(redisTemplate);
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
