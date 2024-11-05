83d49c6d73ee36ae88e1d52e136a1e7c43be3aa6
염규영 <yeomky98@naver.com>
2024-11-05 15:25:27 +0900

    Feat: Add toEntity method to MemberRegisterRequest

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/dto/request/MemberRegisterRequest.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/dto/request/MemberRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.ssafy.s103.domain.member.dto.request;
 
+import com.ssafy.s103.domain.member.entity.Member;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
@@ -9,4 +10,10 @@ public record MemberRegisterRequest(
     @NotNull String password
 ) {
 
+    public Member toEntity(String encodedPassword) {
+        return Member.builder()
+            .email(email)
+            .password(encodedPassword)
+            .build();
+    }
 }
