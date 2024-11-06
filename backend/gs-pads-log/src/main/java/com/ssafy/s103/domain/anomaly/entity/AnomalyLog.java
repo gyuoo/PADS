aaf8c2bdd96a6fe6fb86c07517646901c226a8d0
gyeong_min <nkh9587@naver.com>
2024-11-06 16:15:04 +0900

    Fix: 데이터 구조 변경

--- backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/entity/AnomalyLog.java
+++ backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/entity/AnomalyLog.java
@@ -1,14 +1,18 @@
 package com.ssafy.s103.domain.anomaly.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,22 +27,19 @@ public class AnomalyLog {
 	private Long id;
 
 	@Column(nullable = false)
-	private Long productId;
+	private String productId;
 
-	@Column(nullable = false)
-	private String anomalyCode;
-
-	@Column(nullable = false)
-	private Integer anomalyScore;
+	@OneToMany(mappedBy = "anomalyLog", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<AnomalyReport> anomalyReports = new ArrayList<>();
 
 	@CreationTimestamp
-	@Column(updatable = false)
+	@Column(columnDefinition = "timestamp(0)", updatable = false)
 	private LocalDateTime createdAt;
 
 	@Builder
-	public AnomalyLog(Long productId, String anomalyCode, Integer anomalyScore) {
-		this.anomalyCode = anomalyCode;
+	public AnomalyLog(String productId, List<AnomalyReport> anomalyReports) {
 		this.productId = productId;
-		this.anomalyScore = anomalyScore;
+		if (anomalyReports != null)
+			this.anomalyReports = anomalyReports;
 	}
 }
