68cd2677220a993b5f8169be78c89df467249620
gyeong_min <nkh9587@naver.com>
2024-11-08 11:02:52 +0900

    Feat: AnomalyLog와 AnomalyReport 저장 로직 추가

--- backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/application/service/KafkaConsumerService.java
+++ backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/application/service/KafkaConsumerService.java
@@ -5,7 +5,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.s103.domain.anomaly.application.repository.AnomalyLogRepository;
+import com.ssafy.s103.domain.anomaly.application.repository.AnomalyReportRepository;
 import com.ssafy.s103.domain.anomaly.dto.request.AnomalyLogCreateRequest;
+import com.ssafy.s103.domain.anomaly.entity.AnomalyLog;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,13 +18,16 @@ import lombok.extern.slf4j.Slf4j;
 public class KafkaConsumerService {
 
 	private final AnomalyLogRepository anomalyLogRepository;
+	private final AnomalyReportRepository anomalyReportRepository;
 	private final ObjectMapper objectMapper;
 
 	@KafkaListener(topics = "${spring.kafka.topic.log-topic}", groupId = "${spring.kafka.consumer.group-id}")
 	public void listenLogRequest(ConsumerRecord<String, String> record) {
 		try {
 			AnomalyLogCreateRequest anomalyLogRequest = objectMapper.readValue(record.value(), AnomalyLogCreateRequest.class);
-			anomalyLogRepository.save(anomalyLogRequest.toEntity());
+			AnomalyLog anomalyLog = anomalyLogRequest.toAnomalyLog();
+			anomalyLogRepository.save(anomalyLog);
+			anomalyReportRepository.saveAll(anomalyLogRequest.toAnomalyReports(anomalyLog));
 
 			log.info("받은 메시지: " + record.value());
 		} catch (Exception e) {
