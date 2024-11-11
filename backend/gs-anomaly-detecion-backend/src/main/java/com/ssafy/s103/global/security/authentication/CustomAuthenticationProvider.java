37f3ebfeae95f0b18a4103b8eaf5cec1516bcde3
염규영 <yeomky98@naver.com>
2024-11-11 14:11:55 +0900

    Refactor: Remove unnecessary codes

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/authentication/CustomAuthenticationProvider.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/global/security/authentication/CustomAuthenticationProvider.java
@@ -1,12 +1,9 @@
 package com.ssafy.s103.global.security.authentication;
 
-import static com.ssafy.s103.global.exception.ErrorCode.INVALID_PASSWORD;
-
 import com.ssafy.s103.domain.member.application.service.MemberDetailsService;
 import com.ssafy.s103.domain.member.entity.CustomUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,10 +24,6 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         String rawPassword = (String) authentication.getCredentials();
 
         CustomUser member = (CustomUser) memberDetailsService.loadUserByUsername(email);
-        
-        if (!bCryptPasswordEncoder.matches(rawPassword, member.getPassword())) {
-            throw new BadCredentialsException(INVALID_PASSWORD.getMessage());
-        }
 
         return new CustomAuthenticationToken(member, null, member.getAuthorities());
     }
