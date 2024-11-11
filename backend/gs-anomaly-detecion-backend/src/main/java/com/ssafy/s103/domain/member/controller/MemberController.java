cfc6c6489b5dacd1cdaa89e1a5f6511676f5b052
염규영 <yeomky98@naver.com>
2024-11-11 14:11:49 +0900

    Refactor: Remove unnecessary codes

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/controller/MemberController.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/controller/MemberController.java
@@ -4,13 +4,8 @@ import com.ssafy.s103.domain.member.application.service.MemberService;
 import com.ssafy.s103.domain.member.dto.request.MemberRegisterRequest;
 import com.ssafy.s103.global.exception.SuccessCode;
 import com.ssafy.s103.global.response.ApiResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,13 +23,5 @@ public class MemberController {
         memberService.save(request);
         return ApiResponse.of(SuccessCode.REGISTER_SUCCESS.getMessage(),
             SuccessCode.REGISTER_SUCCESS.getCode(), null);
-    }
-
-    @GetMapping("/logout")
-    public ApiResponse<Void> logout(HttpServletRequest request, HttpServletResponse response) {
-        new SecurityContextLogoutHandler().logout(request, response,
-            SecurityContextHolder.getContext().getAuthentication());
-        return ApiResponse.of(SuccessCode.LOGOUT_SUCCESS.getMessage(),
-            SuccessCode.LOGOUT_SUCCESS.getCode(), null);
     }
 }
