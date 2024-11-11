92c58579d652b8828cb8f56daadbd346a0ff5d2d
염규영 <yeomky98@naver.com>
2024-11-11 17:16:12 +0900

    Refactor: Remove unnecessary code

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/dto/request/MemberLoginRequest.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/dto/request/MemberLoginRequest.java
@@ -1,6 +1,5 @@
 package com.ssafy.s103.domain.member.dto.request;
 
-import com.ssafy.s103.domain.member.entity.Member;
 import jakarta.validation.constraints.NotNull;
 
 public record MemberLoginRequest(
@@ -8,10 +7,4 @@ public record MemberLoginRequest(
     @NotNull String password
 ) {
 
-    public Member toEntity(String encodedPassword) {
-        return Member.builder()
-            .email(email)
-            .password(encodedPassword)
-            .build();
-    }
 }
