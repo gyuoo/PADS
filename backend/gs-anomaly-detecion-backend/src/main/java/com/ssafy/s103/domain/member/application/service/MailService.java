f192be49661f7b61a15115585bd52f8ba80e25ab
염규영 <yeomky98@naver.com>
2024-11-12 17:03:18 +0900

    Refactor: Remove unnecessary codes

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/application/service/MailService.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/application/service/MailService.java
@@ -2,6 +2,7 @@ package com.ssafy.s103.domain.member.application.service;
 
 import static com.ssafy.s103.global.exception.ErrorCode.EMAIL_SEND_ERROR;
 
+import com.ssafy.s103.global.util.RedisUtil;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -15,27 +16,33 @@ import org.springframework.stereotype.Service;
 public class MailService {
 
     private final JavaMailSender javaMailSender;
+    private final RedisUtil redisUtil;
 
     @Value("${spring.mail.username}")
     private String senderEmail;
 
     // 랜덤으로 숫자 생성
-    public int createNumber() {
+    private int createNumber() {
         return (int) (Math.random() * 900000) + 100000;
     }
 
-    public MimeMessage createMail(String mail, int code) {
+    private MimeMessage createMail(String email) {
+        String code = String.valueOf(createNumber());
         MimeMessage message = javaMailSender.createMimeMessage();
 
         try {
             message.setFrom(senderEmail);
-            message.setRecipients(MimeMessage.RecipientType.TO, mail);
+            message.setRecipients(MimeMessage.RecipientType.TO, email);
             message.setSubject("[Pads] 인증 번호 안내 드립니다.");
-            String body = "";
-            body += "<h3>" + "요청하신 인증 번호입니다. 아래 번호를 입력해 주세요." + "</h3>";
-            body += "<h1>" + code + "</h1>";
-            body += "<h3>" + "감사합니다." + "</h3>";
+            String body = """
+                <h3>요청하신 인증 번호입니다. 아래 번호를 입력해 주세요.</h3>
+                <h1>%s</h1>
+                <h3>감사합니다.</h3>
+                """.formatted(code);
             message.setText(body, "UTF-8", "html");
+
+            // Redis 에 해당 인증코드 인증 시간 설정
+            redisUtil.setDataExpire(email, code, 60 * 10L);
         } catch (MessagingException e) {
             e.printStackTrace();
         }
@@ -43,15 +50,24 @@ public class MailService {
         return message;
     }
 
-    public int sendMail(String mail) {
-        int code = createNumber();
-        MimeMessage message = createMail(mail, code);
+    public void sendMail(String mail) {
+        if (redisUtil.existData(mail)) {
+            redisUtil.deleteData(mail);
+        }
+        MimeMessage message = createMail(mail);
         try {
             javaMailSender.send(message); // 메일 발송
         } catch (MailException e) {
             e.printStackTrace();
             throw new IllegalArgumentException(EMAIL_SEND_ERROR.getMessage());
         }
-        return code;
+    }
+
+    public Boolean verifyEmailCode(String email, String code) {
+        String codeFoundByEmail = redisUtil.getData(email);
+        if (codeFoundByEmail == null) {
+            return false;
+        }
+        return codeFoundByEmail.equals(code);
     }
 }
--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/controller/MailController.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/member/controller/MailController.java
@@ -3,8 +3,6 @@ package com.ssafy.s103.domain.member.controller;
 import com.ssafy.s103.domain.member.application.service.MailService;
 import com.ssafy.s103.domain.member.dto.request.MemberEmailVerifyRequest;
 import com.ssafy.s103.domain.member.dto.request.MemberVerificationCodeRequest;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,26 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/mails")
 public class MailController {
 
-    private final Map<String, String> verificationCodes = new HashMap<>();
     private final MailService mailService;
-    private int verificationCode; // 이메일 인증 번호
 
     // 인증 이메일 전송
     @PostMapping("/send")
     public ResponseEntity<Void> sendMail(
         @RequestBody MemberEmailVerifyRequest memberEmailVerifyRequest) {
-        HashMap<String, Object> map = new HashMap<>();
-        try {
-            verificationCode = mailService.sendMail(memberEmailVerifyRequest.email());
-            String code = String.valueOf(verificationCode);
-            verificationCodes.put(memberEmailVerifyRequest.email(), code);
-            map.put("success", Boolean.TRUE);
-            map.put("verificationCode", code);
-        } catch (Exception e) {
-            map.put("success", Boolean.FALSE);
-            map.put("error", e.getMessage());
-        }
-
+        mailService.sendMail(memberEmailVerifyRequest.email());
         return ResponseEntity.ok().build();
     }
 
@@ -45,19 +30,14 @@ public class MailController {
     @GetMapping("/check")
     public ResponseEntity<Void> checkMail(
         @RequestBody MemberVerificationCodeRequest memberVerificationCodeRequest) {
-
-        boolean hasEmail = verificationCodes.containsKey(memberVerificationCodeRequest.email());
-        if (!hasEmail) {
-            return ResponseEntity.notFound().build();
-        }
-
-        boolean isMatch = memberVerificationCodeRequest.code().equals(String.valueOf(
-            verificationCode));
-        if (!isMatch) {
+        String email = memberVerificationCodeRequest.email();
+        String code = memberVerificationCodeRequest.code();
+        
+        boolean isVerify = mailService.verifyEmailCode(email, code);
+        if (!isVerify) {
             return ResponseEntity.badRequest().build();
         }
 
-        verificationCodes.remove(memberVerificationCodeRequest.email());
         return ResponseEntity.ok().build();
     }
 }
