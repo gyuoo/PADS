953d0fc9338427487f390915b73706293e54cb16
gyeong_min <nkh9587@naver.com>
2024-11-12 15:40:28 +0900

    Refactor : 상수 활용

--- backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/dto/request/AnomalyLogCreateRequest.java
+++ backend/gs-pads-log/src/main/java/com/ssafy/s103/domain/anomaly/dto/request/AnomalyLogCreateRequest.java
@@ -14,6 +14,10 @@ public record AnomalyLogCreateRequest(
 	@JsonProperty("total_score") Integer totalScore,
 	@JsonProperty("report") Map<String, String> report
 ) {
+
+	private static final String SCORE_SUFFIX = "_score";
+	private static final String MESSAGE_SUFFIX = "_message";
+
 	public AnomalyLog toAnomalyLog() {
 		return AnomalyLog.builder()
 			.productId(productId)
@@ -27,11 +31,11 @@ public record AnomalyLogCreateRequest(
 		Map<String, String> messages = new HashMap<>();
 
 		report.forEach((key, value) -> {
-			if (key.endsWith("_score")) {
-				String code = key.substring(0, key.lastIndexOf("_score"));
+			if (key.endsWith(SCORE_SUFFIX)) {
+				String code = key.substring(0, key.lastIndexOf(SCORE_SUFFIX));
 				scores.put(code, Integer.valueOf(value));
-			} else if (key.endsWith("_message")) {
-				String code = key.substring(0, key.lastIndexOf("_message"));
+			} else if (key.endsWith(MESSAGE_SUFFIX)) {
+				String code = key.substring(0, key.lastIndexOf(MESSAGE_SUFFIX));
 				messages.put(code, value);
 			}
 		});
