4a8a7e4d2eb30a06e2ebb21679dcde8afe733520
Gyuyoung <yeomky98@naver.com>
2024-11-13 17:14:53 +0900

    Feat: Add basic entities

---
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalyproduct/entity/AnomalyStatus.java
@@ -0,0 +1,17 @@
+package com.ssafy.s103.domain.anomalyproduct.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum AnomalyStatus {
+    SCHEDULED("예정"),      // 작업이 예정된 상태
+    WAIT("대기"),
+    PROGRESS("진행중"),  // 작업이 진행 중인 상태
+    COMPLETED("완료"),      // 작업이 완료된 상태
+    FAILED("실패");         // 작업이 실패한 상태
+    private final String description;
+
+    AnomalyStatus(String description) {
+        this.description = description;
+    }
+}
