059a7799de5764269c816fbca197d38171a946bf
방태연 <imbhang3@naver.com>
2024-11-13 10:50:57 +0900

    Fix : 도커라이징된 버전에 맞게 수정

--- data/gs-anomaly-detecion-data/product_consumer.py
+++ data/gs-anomaly-detecion-data/product_consumer.py
@@ -1,49 +1,80 @@
-from dotenv import load_dotenv
 from kafka import KafkaConsumer, KafkaProducer
 import logging
 import multiprocessing
 import os
 import json
+import boto3
+import pandas as pd
+from io import StringIO
+from pydantic import BaseModel, ValidationError
+from model import AnomalyDetector
 
-from random import randint
 
-# 환경변수 load
-load_dotenv()
+class DetectionReqeust(BaseModel):
+    batch_id: int
+    csv_path: str
+    image_path: str
 
-# Kafka Producer config
-bootstrap_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+
+# 환경 변수 세팅
+bootstrap_servers = os.getenv("KAFKA_BOOTSTRAP_SERVERS").split(',')
+kafka_product_topic = os.getenv('PADS_PRODUCT_TOPIC')
+kafka_log_topic = os.getenv('PADS_LOG_TOPIC')
+kafka_ack_topic = os.getenv('PADS_ACK_TOPIC')
+
+# Kafka Producer 세팅
 producer = KafkaProducer(
-    bootstrap_servers=[bootstrap_servers],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # JSON 직렬화 설정
+    bootstrap_servers=bootstrap_servers,
+    value_serializer=lambda v: json.dumps(v, ensure_ascii=False).encode('utf-8')  # JSON 직렬화 설정
 )
 
-kafka_log_topic = os.getenv('PADS_LOG_TOPIC')
-
+# 로거 설정
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler = logging.FileHandler('product_consumer.log', encoding='utf-8')
 file_handler.setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
 logger.addHandler(file_handler)
+
+# S3 client 설정
+s3_bucket = os.getenv('S3_BUCKET')
+s3_key = os.getenv('CREDENTIALS_ACCESS_KEY')
+s3_secret_key = os.getenv('CREDENTIALS_SECRET_KEY')
+s3_region = os.getenv('S3_REGION')
+s3_client = boto3.client(service_name='s3',
+                         aws_access_key_id=s3_key,
+                         aws_secret_access_key=s3_secret_key,
+                         region_name=s3_region)
+
+# Detector Import
+anomaly_detector = AnomalyDetector()
 
 
 def send_json_message(topic, message, key=None):
     """Kafka 토픽에 JSON 메시지를 전송"""
     key = key.encode('utf-8') if key else None
-    producer.send(topic, key=key, value=message)
-    producer.flush()
+    resp = producer.send(topic, key=key, value=message).get(timeout=10)
+    logger.info(resp)
+
+
+def process_csv_from_s3(request: DetectionReqeust):
+    response = s3_client.get_object(Bucket=s3_bucket, Key=request.csv_path)
+    csv_content = response['Body'].read().decode('utf-8')
+    data = pd.read_csv(StringIO(csv_content))
+    return anomaly_detector.process(data)
+
+
+def data_to_log_mapping(row):
+    row_data = row.drop('prd_id').to_dict()
+    json_str = json.dumps(row_data, ensure_ascii=False)
+    return json_str
 
 
 def start_consumer(consumer_id):
 
-    topic_name = os.getenv('PADS_PRODUCT_TOPIC')
-    log_topic_name = os.getenv('PADS_LOG_TOPIC')
-    bootstrap_servers = [os.getenv('KAFKA_BOOTSTRAP_SERVERS')]
-
     consumer = KafkaConsumer(
-        topic_name,
+        kafka_product_topic,
         bootstrap_servers=bootstrap_servers,
         auto_offset_reset='earliest',
         enable_auto_commit=True,
@@ -55,40 +86,34 @@ def start_consumer(consumer_id):
     for message in consumer:
         data = json.loads(message.value.decode('utf-8'))
 
-        logger.info(f"Consumer {consumer_id} - 받은 메시지: {data}")
-        # TO_DO : 데이터 처리 로직
-        if 'prd_id' not in data:
-            logger.error(f"Consumer {consumer_id} - KeyError: {data}")
-            # TO_DO : 예외시 처리 로직
-            
+        try:
+            detection_request = DetectionReqeust(**data)
+        except ValidationError as e:
+            logger.error("Validation 실패 : ", e.json())
+            continue
+        except json.JSONDecodeError:
+            logger.error("Json format 오류")
             continue
 
-        # Log Topic에 전송
-        json_message = {
-            "prd_id": data["prd_id"],
-            "report": {
-                "A001": randint(0, 100),
-                "A002": randint(0, 100),
-                "A003": randint(0, 100),
-                "A004": randint(0, 100)
-            },
-            "status": "received"
+        ack_message = {
+            "batch_id": detection_request.batch_id
         }
+        print(ack_message)
+        send_json_message(kafka_ack_topic, ack_message)
+        logger.info(f"Consumer {consumer_id} - 받은 메시지: {data}")
 
-        send_json_message(log_topic_name, json_message)
-        logger.info(f"Consumer {consumer_id} - JSON 메시지 전송 완료: {json.dumps(json_message, ensure_ascii=False)}")
+        resp_data = process_csv_from_s3(detection_request)
+
+        for _, row in resp_data.iterrows():
+            json_message = {
+                "prd_id": row.get("prd_id"),
+                "report": data_to_log_mapping(row),
+                "status": "processed"
+            }
+            send_json_message(kafka_log_topic, json_message)
+            logger.debug(f"{kafka_log_topic} - {json_message}")
+        logger.info("row 처리 완료")
 
 
 if __name__ == '__main__':
-    consumer_processes = []
-
-    available_cpu_cores = os.cpu_count()
-
-    for i in range(3):
-        if i < available_cpu_cores:
-            process = multiprocessing.Process(target=start_consumer, args=(i+1,))
-            consumer_processes.append(process)
-            process.start()
-
-    for process in consumer_processes:
-        process.join()
+    start_consumer(1)
