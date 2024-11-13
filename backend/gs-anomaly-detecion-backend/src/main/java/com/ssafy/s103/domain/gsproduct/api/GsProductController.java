37a58ac568f87bccdf60b0b56eb7c458e00fca52
Gyuyoung <yeomky98@naver.com>
2024-11-13 15:19:17 +0900

    Feat: Add method to retrieve all products

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/gsproduct/api/GsProductController.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/gsproduct/api/GsProductController.java
@@ -1,7 +1,11 @@
 package com.ssafy.s103.domain.gsproduct.api;
 
 import com.ssafy.s103.domain.gsproduct.application.service.GsProductService;
+import com.ssafy.s103.domain.gsproduct.entity.GsShopProduct;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,9 @@ public class GsProductController {
 
     private final GsProductService gsProductService;
 
+    @GetMapping
+    public ResponseEntity<List<GsShopProduct>> getAllProducts() {
+        List<GsShopProduct> result = gsProductService.getAllProducts();
+        return ResponseEntity.ok(result);
+    }
 }
