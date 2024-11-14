08a8e32bfcf081ccf9241665ca076234e897b24d
Gyuyoung <yeomky98@naver.com>
2024-11-14 14:30:52 +0900

    Refactor: Combine multiple queries into a single query

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalyproduct/application/repository/AnomalyProductRepository.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalyproduct/application/repository/AnomalyProductRepository.java
@@ -1,25 +1,17 @@
 package com.ssafy.s103.domain.anomalyproduct.application.repository;
 
 import com.ssafy.s103.domain.anomalyproduct.entity.AnomalyProduct;
-import com.ssafy.s103.domain.anomalyproduct.entity.AnomalyStatus;
-import java.time.LocalDateTime;
+import com.ssafy.s103.domain.batch.dto.response.BatchResponse;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AnomalyProductRepository extends JpaRepository<AnomalyProduct, Long> {
 
-    int countByStatus(AnomalyStatus status);
-
-    @Query("SELECT apb.createDt FROM AnomalyProductBatch apb WHERE apb.id = :batchId")
-    LocalDateTime findCreateDtByBatchId(@Param("batchId") String batchId);
-
-    @Query("SELECT ap.batchId FROM AnomalyProduct ap WHERE ap.prdId = :prdId")
-    String findBatchIdByPrdId(Long prdId);
-
-    @Query("SELECT ap.status FROM AnomalyProduct ap WHERE ap.prdId = :prdId")
-    String findStatusByPrdId(@Param("prdId") Long prdId);
-
-    @Query("SELECT ap.viewName FROM AnomalyProduct ap WHERE ap.prdId = :prdId")
-    String findViewNameByPrdId(@Param("prdId") Long prdId);
+    @Query("SELECT new com.ssafy.s103.domain.batch.dto.response.BatchResponse(" +
+        "ap.prdId, ap.viewName, apb.createDt, ap.status) " +
+        "FROM AnomalyProduct ap " +
+        "JOIN AnomalyProductBatch apb ON ap.batchId = apb.id " +
+        "WHERE ap.prdId = :prdId")
+    BatchResponse findBatchResponseByPrdId(@Param("prdId") Long prdId);
 }
