9db1f676c5a3ae4000f31f5673a9a15b4e720418
Gyuyoung <yeomky98@naver.com>
2024-11-14 09:09:29 +0900

    Refactor: Remove unnecessary code

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalyproduct/application/repository/AnomalyProductRepository.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalyproduct/application/repository/AnomalyProductRepository.java
@@ -3,10 +3,8 @@ package com.ssafy.s103.domain.anomalyproduct.application.repository;
 import com.ssafy.s103.domain.anomalyproduct.entity.AnomalyProduct;
 import com.ssafy.s103.domain.anomalyproduct.entity.AnomalyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AnomalyProductRepository extends JpaRepository<AnomalyProduct, Integer> {
 
-    @Query("SELECT COUNT(ap) FROM AnomalyProduct ap WHERE ap.status = :status")
     int countByStatus(AnomalyStatus status);
 }
