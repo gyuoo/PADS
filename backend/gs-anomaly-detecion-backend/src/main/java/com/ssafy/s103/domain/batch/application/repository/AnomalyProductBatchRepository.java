9153ab22b39e3372fe09ce77db53c2e8652a1198
Gyuyoung <yeomky98@naver.com>
2024-11-14 13:39:49 +0900

    Refactor: Remove unused repository

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/batch/application/repository/AnomalyProductBatchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.s103.domain.batch.application.repository;
-
-import com.ssafy.s103.domain.batch.entity.AnomalyProductBatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnomalyProductBatchRepository extends JpaRepository<AnomalyProductBatch, Long> {
-    
-}
