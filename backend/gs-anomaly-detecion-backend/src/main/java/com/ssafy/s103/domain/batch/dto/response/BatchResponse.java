5c9ffedd7a768d4ce290fa33556ad414c73602a7
Gyuyoung <yeomky98@naver.com>
2024-11-14 14:10:12 +0900

    Feat: Combine detail and preview DTOs

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/batch/dto/response/BatchResponse.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/batch/dto/response/BatchResponse.java
@@ -1,14 +1,15 @@
 package com.ssafy.s103.domain.batch.dto.response;
 
+import com.ssafy.s103.domain.anomalyproduct.entity.AnomalyStatus;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
-public record BatchDetailsResponse(
-    Long proudctId,
+public record BatchResponse(
+    Long productId,
     String productName,
     LocalDateTime startDateTime,
-    String status
+    AnomalyStatus status
 ) {
 
 }
