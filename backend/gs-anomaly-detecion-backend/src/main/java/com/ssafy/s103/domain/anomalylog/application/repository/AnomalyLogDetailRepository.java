676eb1dc77b1edbe9b3eff2178e46b2fc06d0f56
gyeong_min <nkh9587@naver.com>
2024-11-16 01:16:33 +0900

    Feat : 데이터 정렬 추가

--- backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalylog/application/repository/AnomalyLogDetailRepository.java
+++ backend/gs-anomaly-detecion-backend/src/main/java/com/ssafy/s103/domain/anomalylog/application/repository/AnomalyLogDetailRepository.java
@@ -3,10 +3,12 @@ package com.ssafy.s103.domain.anomalylog.application.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ssafy.s103.domain.anomalylog.entity.AnomalyLog;
 import com.ssafy.s103.domain.anomalylog.entity.AnomalyLogDetail;
 
 public interface AnomalyLogDetailRepository extends JpaRepository<AnomalyLogDetail, Long> {
+	@Query("SELECT a FROM AnomalyLogDetail a WHERE a.anomalyLog = :anomalyLog ORDER BY a.code ASC, a.subCode ASC")
 	List<AnomalyLogDetail> findByAnomalyLog(AnomalyLog anomalyLog);
 }
