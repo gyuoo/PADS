64a9799d05f6966d94a76d24c809637adfd6b602
gyeong_min <nkh9587@naver.com>
2024-11-18 16:30:34 +0900

    Fix : 로딩스피너

--- frontend/gs-ads-dashboard-front/src/components/dashboard/AnomalyProductList.vue
+++ frontend/gs-ads-dashboard-front/src/components/dashboard/AnomalyProductList.vue
@@ -6,13 +6,13 @@
     </div>
     <div class="text-sm text-gray-500 mb-5 px-2">{{ today }}</div>
     <div v-if="isLoading" class="flex justify-center items-center h-[500px]">
-      <svg class="animate-spin h-8 w-8 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none"
-        viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
-      </svg>
-    </div>
-    <div class="overflow-x-auto h-6/7">
+    <svg class="animate-spin h-8 w-8 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+      viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
+    </svg>
+  </div>
+    <div v-else class="overflow-x-auto h-6/7">
       <table class="min-w-full rounded-lg">
         <thead>
           <tr>
